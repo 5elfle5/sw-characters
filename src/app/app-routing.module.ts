352ca4c61e98d5ae732c8a23927ab4f5cf017d13
Andrii Kapinos <broxbeats@gmail.com>
2020-12-09 09:12:44 +0200

    added characters module 2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'characters', loadChildren: () => import('./characters/characters/characters.module').then(m => m.CharactersModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
